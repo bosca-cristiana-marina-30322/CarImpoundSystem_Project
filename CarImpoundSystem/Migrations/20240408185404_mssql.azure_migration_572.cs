ccd1af4d5bf413846cb8d152f79a539f7942a570
bosca-cristiana-marina-30322 <Bosca.Io.Cristiana@student.utcluj.ro>
2024-04-08 22:40:13 +0300

    DbConnection

---
+++ CarImpoundSystem/Migrations/20240408185404_mssql.azure_migration_572.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarImpoundSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqlazure_migration_572 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
