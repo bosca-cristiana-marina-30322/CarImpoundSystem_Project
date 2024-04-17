5ab5958e7bf7d3fb0419712ea034cebeb8649e54
bosca-cristiana-marina-30322 <Bosca.Io.Cristiana@student.utcluj.ro>
2024-04-17 11:35:43 +0300

    Added Db to login

--- CarImpoundSystem/Controllers/WorkerController.cs
+++ CarImpoundSystem/Controllers/WorkerController.cs
@@ -1,4 +1,5 @@
-﻿using CarImpoundSystem.Models;
+﻿using CarImpoundSystem.Data;
+using CarImpoundSystem.Models;
 using CarImpoundSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +7,10 @@ namespace CarImpoundSystem.Controllers
 {
     public class WorkerController : Controller
     {
-        private readonly AuthService authService;
-        public WorkerController(AuthService authService)
-        {
-            this.authService = authService;
+        
+        private readonly AppDBContext _context;
+        public WorkerController(AppDBContext context) {
+            _context = context;
         }
         public ActionResult RegisterPoundedVehicle(Vehicle vehicle)
         {
@@ -17,32 +18,15 @@ namespace CarImpoundSystem.Controllers
             return View();
         }
 
-        public IActionResult Index()
-        {
-            // Check if user is authenticated
-            if (!User.Identity.IsAuthenticated)
-            {
-                // Redirect to worker login page
-                return RedirectToAction("Login", "Worker");
-
-            }
-
-            // Check if user has the correct role
-            if (!authService.HasRole(User.Identity.Name, "Worker"))
-            {
-                // Unauthorized access
-                return RedirectToAction("Unauthorized", "Account");
-            }
-
-            return View();
-        }
+       
 
         [HttpGet]
         public IActionResult Login(string username, string password)
         {
+            var user = _context.users.FirstOrDefault(u => u.username == username);
             // Perform authentication here (e.g., check credentials against database)
             // For simplicity, let's assume username is "admin" and password is "password"
-            if (username == "admin" && password == "password")
+            if (user != null && user.password == password && user.role == "worker")
             {
                 // Authentication successful, redirect to index page
                 // You may also want to implement actual authentication logic here
